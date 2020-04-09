ca6b0b6a11c556b3da2d90cb3f24c8fceb7920c0
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-04-10 00:19:18 +0900

    :gear: configured for use headless chrome.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'capybara/rspec'
+require 'webdrivers/chromedriver'
 require 'travel_to_javascript'
 
 RSpec.configure do |config|
@@ -14,3 +16,20 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+Capybara.register_driver :headless_chrome do |app|
+  driver = Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      login_prefs: { browser: 'ALL' },
+      chrome_options: {
+        args: %w[headless disable-gpu window-size=1900,1200 lang=ja no-sandbox disable-dev-shm-usage],
+      }
+    )
+  )
+  driver
+end
+
+Capybara.server = :webrick
+Capybara.javascript_driver = :headless_chrome
