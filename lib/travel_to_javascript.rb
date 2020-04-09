79609ff203f422195252b71a1c79a132100371b8
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2020-04-10 00:19:52 +0900

    :sparkles: add main logic for travel_to_javascript.

--- lib/travel_to_javascript.rb
+++ lib/travel_to_javascript.rb
@@ -3,6 +3,47 @@
 require 'travel_to_javascript/version'
 
 module TravelToJavascript
-  class Error < StandardError; end
-  # Your code goes here...
+  def travel_to_javascript(page, datetime)
+    page.execute_script time_stop_javascript(datetime)
+    yield
+    page.execute_script time_undo_javsctipt
+  end
+
+  private
+
+  def time_stop_javascript(rb_datetime)
+    <<~JS
+      originDate = Date;
+      Date = #{time_stop_js_function_for_date(rb_datetime)};
+      Date.now = #{time_stop_js_function_for_date_now(rb_datetime)};
+    JS
+  end
+
+  def time_undo_javsctipt
+    'Date = originDate;'
+  end
+
+  def time_stop_js_function_for_date(rb_datetime)
+    <<~JS
+      function (datetime) {
+        if (datetime) {
+          return new originDate(datetime);
+        } else {
+          return new originDate("#{rb_datetime.iso8601(6)}");
+        }
+      }
+    JS
+  end
+
+  def time_stop_js_function_for_date_now(rb_datetime)
+    <<~JS
+      function (datetime) {
+        if (datetime) {
+          return new originDate(datetime).getTime();
+        } else {
+          return new originDate("#{rb_datetime.iso8601(6)}").getTime();
+        }
+      }
+    JS
+  end
 end
--- spec/travel_to_javascript_spec.rb
+++ spec/travel_to_javascript_spec.rb
@@ -1,7 +1,68 @@
 # frozen_string_literal: true
 
-RSpec.describe TravelToJavascript do
+require 'test_app'
+require 'time'
+
+RSpec.describe TravelToJavascript, type: :feature do
+  include TravelToJavascript
+  class JsConsoleDates
+    JS_NEW_DATE_REGXP = /[A-Z].+/.freeze
+    JS_DATE_NOW_REGXP = /[0-9]{5,}.+/.freeze
+
+    def initialize(messages)
+      @messages = messages
+    end
+
+    def call
+      js_parsed_console
+    end
+
+    private
+
+    attr_reader :messages
+
+    def js_parsed_console
+      messages.select { |msg| msg.start_with?('console-api') }
+              .map(&method(:match_data))
+              .map(&method(:parsed_match_data))
+              .map(&:to_s)
+              .each_slice(2)
+              .to_a
+    end
+
+    def match_data(msg)
+      (msg.match(JS_NEW_DATE_REGXP) || msg.match(JS_DATE_NOW_REGXP))[0].to_s
+    end
+
+    def parsed_match_data(match_data)
+      if match_data.match?(JS_NEW_DATE_REGXP)
+        DateTime.parse(match_data)
+      else
+        Time.at(match_data.to_i / 1000).to_datetime
+      end
+    end
+  end
+
+  def call_js_get_date(session)
+    session.execute_script('console.error(new Date())')
+    session.execute_script('console.error(Date.now())')
+  end
+
+  let(:target_date) { DateTime.parse('2000-01-01 1:11:11.111+9:00') }
+  let(:session) { Capybara::Session.new(:headless_chrome, TestApp) }
+
   it 'has a version number' do
     expect(TravelToJavascript::VERSION).not_to be nil
+  end
+
+  it 'locks time in javascript and restore time outside block.' do
+    session.visit('/')
+    call_js_get_date(session)
+    travel_to_javascript(session, target_date) { call_js_get_date(session) }
+    call_js_get_date(session)
+    messages = session.driver.browser.manage.logs.get(:browser).collect(&:message)
+    result = JsConsoleDates.new(messages).call
+    expect(result[1]).to eq [target_date.to_s, target_date.to_s]
+    expect(result[0]).to eq result[2]
   end
 end
